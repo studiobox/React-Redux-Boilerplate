0195b7072381172a3da99a994b0a6761f449921c
studiobox <kalpak.palekar@gmail.com>
2017-01-16 18:31:58 +0530

    First commit

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,28 @@
+export default function() {
+    return [
+        {
+            id: 1,
+            first: "Bucky",
+            last: "Roberts",
+            age: 71,
+            description: "Bucky is a react developer and Youtuber",
+            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+        },
+        {
+            id: 2,
+            first: "Joby",
+            last: "Wasilenko",
+            age: 27,
+            description: "Joby loves the Packers, cheese, and turtles.",
+            thumbnail: "http://i.imgur.com/52xRlm8.jpg"
+        },
+        {
+            id: 3,
+            first: "Madison",
+            last: "Williams",
+            age: 24,
+            description: "Madi likes her dog but it is really annoying.",
+            thumbnail: "http://i.imgur.com/4EMtxHB.jpg"
+        }
+    ]
+}
